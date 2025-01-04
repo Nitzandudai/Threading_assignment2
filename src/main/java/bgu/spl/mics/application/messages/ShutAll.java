015d8412f0f8d76366d76c23214a00512e210dd1
Nitzandudai <Nitzandudai@gmail.com>
2025-01-04 16:51:18 +0200

    fj

--- src/main/java/bgu/spl/mics/application/messages/ShutAll.java
+++ src/main/java/bgu/spl/mics/application/messages/ShutAll.java
@@ -3,10 +3,6 @@ package bgu.spl.mics.application.messages;
 import bgu.spl.mics.Broadcast;
 
 public class ShutAll implements Broadcast {
-    private String output;
 
-    public ShutAll (){
-        this.output = "Time Is Up!";
-    }
-
+    public ShutAll (){}
 }
